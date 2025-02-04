63b075b49813529c605c654ffb4f105542bdf08a
Nichesco <80578484+Nichesco@users.noreply.github.com>
2025-02-04 16:09:52 +0100

    Update config.js

--- config.js
+++ config.js
@@ -16,17 +16,17 @@ module.exports = {
   errorLog: "", 
   nodes: [
     {
-      name: "INZEWORLD.COM (DE)",
-      password: "saher.inzeworld.com",
-      host: "lava.inzeworld.com",
-      port: 3128,
-      secure: false
-    },
-    {
       name: "Koi Node V4",
       password: "prplmoe.me",
       host: "lavav4.prplmoe.me",
       port: 1118,
+      secure: false
+    },
+    {
+      name: "INZEWORLD.COM (DE)",
+      password: "saher.inzeworld.com",
+      host: "lava.inzeworld.com",
+      port: 3128,
       secure: false
     },
     {
