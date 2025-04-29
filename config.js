4e2c95f28fb9fd8aaa841a3667e607ab9dd04f4b
Nichesco <80578484+Nichesco@users.noreply.github.com>
2025-04-29 21:52:30 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,14 +3,14 @@
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  ownerID: ["344461223439499265", ""], 
+  mongodbUri : "mongodb+srv://ckiller1233:uw5He5EKy5kKd32b@cluster0.spwhs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "YouTube Music", 
+  activityName: "Didi City Muziku", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/xQF9f9yUEM",
   embedTimeout: 5, 
